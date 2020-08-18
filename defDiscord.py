f287ccc496deb91c78087e38a9f98a2ac80a8227
Jxck-S <JackHSweeney1@gmail.com>
2020-08-17 23:32:11 -0400

    Add Discord message conf, & simplify enables

--- defDiscord.py
+++ defDiscord.py
@@ -3,7 +3,7 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 def sendDis(message):
-    webhook = DiscordWebhook(url=config.get('DISCORD', 'URL'), content=message, username="plane-notify")
+    webhook = DiscordWebhook(url=config.get('DISCORD', 'URL'), content=message, username=config.get('DISCORD', 'USERNAME'))
 
     with open("map.png", "rb") as f:
         webhook.add_file(file=f.read(), filename='map.png')

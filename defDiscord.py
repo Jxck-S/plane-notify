aeede2633d22cde907404c3e3499d66951b85468
Jxck-S <JackHSweeney1@gmail.com>
2020-09-11 21:15:51 -0400

    Inital Multi Version untested

--- defDiscord.py
+++ defDiscord.py
@@ -2,9 +2,9 @@ from discord_webhook import DiscordWebhook
 import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
-def sendDis(message):
+def sendDis(message, map_file_name):
     webhook = DiscordWebhook(url=config.get('DISCORD', 'URL'), content=message, username=config.get('DISCORD', 'USERNAME'))
 
-    with open("map.png", "rb") as f:
+    with open(map_file_name, "rb") as f:
         webhook.add_file(file=f.read(), filename='map.png')
     response = webhook.execute()

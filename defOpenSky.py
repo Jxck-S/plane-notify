83a71c3a03132701b2c5bfb9dd7147bdc5ca063e
Jack Sweeney <JackHSweeney1@gmail.com>
2020-11-22 01:41:34 +0000

    Unused value access removed

--- defOpenSky.py
+++ defOpenSky.py
@@ -7,7 +7,7 @@ def pullOpenSky(planes):
     opens_api = OpenSkyApi(username= None if main_config.get('OPENSKY', 'USERNAME').upper() == "NONE" else main_config.get('OPENSKY', 'USERNAME'), password= None if main_config.get('OPENSKY', 'PASSWORD').upper() == "NONE" else main_config.get('OPENSKY', 'PASSWORD').upper())
     failed = False
     icao_array = []
-    for key, obj in planes.items():
+    for key in planes.keys():
         icao_array.append(key.lower())
     try:
         planeData = opens_api.get_states(time_secs=0, icao24=icao_array)
